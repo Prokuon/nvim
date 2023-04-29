584a36ee477cc5b25776d5150c3941095df4dc45
Prokuon <anguecn@outlook.com>
2023-04-29 17:12:19 +0800

    20230429

--- lua/LuaSnip/all.lua
+++ lua/LuaSnip/all.lua
@@ -343,6 +343,18 @@ return {
     ),
 
     require("luasnip").snippet(
+        {trig = "vb", dscr = "verb"},
+        fmt(
+            [[
+                \verb|<>|
+            ]],
+
+            { i(1, "n") },
+            { delimiters = "<>"}
+        )
+    ),
+
+    require("luasnip").snippet(
         {trig = "frac", dscr = "frac"},
         fmt(
             [[
@@ -397,6 +409,59 @@ return {
             ]],
 
             { i(1, "n"), i(2, "n"), i(3, "n") },
+            { delimiters = "<>"}
+        )
+    ),
+    require("luasnip").snippet(
+        {trig = "block", dscr = "block"},
+        fmt(
+            [[
+                \begin{aligned}
+                    \mathop{\begin{align} 
+                        \,& \,\\
+                        \verb|<>| & -\\
+                        \verb|<>| & -\\
+                        \verb|<>| & -\\
+                        \verb|<>| & -\\
+                        \verb|<>| & -\\
+                        \verb|<>| & -\\
+                        \verb|<>| & -\\
+                        \verb|<>| & -\\
+                        \verb|<>| & -\\
+                        \verb|<>| & -\\
+                    \end{align}}
+
+                    \boxed{ \begin{align} 
+                        \, && \verb|<>| & \,\\
+                        &\verb|<>| & \, && \verb|<>|\\
+                        &\verb|<>| & \, && \verb|<>|\\
+                        &\verb|<>| & \, && \verb|<>|\\
+                        &\verb|<>| & \, && \verb|<>|\\
+                        &\verb|<>| & \, && \verb|<>|\\
+                        &\verb|<>| & \, && \verb|<>|\\
+                        &\verb|<>| & \, && \verb|<>|\\
+                        &\verb|<>| & \, && \verb|<>|\\
+                        &\verb|<>| & \, && \verb|<>|\\
+                        &\verb|<>| & \, && \verb|<>|\\
+                    \end{align}}
+
+                    \mathop{\begin{align} 
+                        \, & \, \\
+                        - & \verb|<>|\\
+                        - & \verb|<>|\\
+                        - & \verb|<>|\\
+                        - & \verb|<>|\\
+                        - & \verb|<>|\\
+                        - & \verb|<>|\\
+                        - & \verb|<>|\\
+                        - & \verb|<>|\\
+                        - & \verb|<>|\\
+                        - & \verb|<>|\\
+                    \end{align}}
+                \end{aligned}
+            ]],
+
+            {i(1, "n"), i(2, "n"), i(3, "n"), i(4, "n"), i(5, "n"), i(6, "n"), i(7, "n"), i(8, "n"), i(9, "n"), i(10, "n"), i(11, "n"), i(12, "n"), i(13, "n"), i(14, "n"), i(15, "n"), i(16, "n"), i(17, "n"), i(18, "n"), i(19, "n"), i(20, "n"), i(21, "n"), i(22, "n"), i(23, "n"), i(24, "n"), i(25, "n"), i(26, "n"), i(27, "n"), i(28, "n"), i(29, "n"), i(30, "n"), i(31, "n"), i(32, "n"), i(33, "n"), i(34, "n"), i(35, "n"), i(36, "n"), i(37, "n"), i(38, "n"), i(39, "n"), i(40, "n"), i(41, "n")},
             { delimiters = "<>"}
         )
     )
