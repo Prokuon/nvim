39ee96151d9cfd70a09a6f46ffba453aedfac028
Prokuon <anguecn@outlook.com>
2023-05-10 11:38:41 +0800

    20230508

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -19,49 +19,8 @@
 --tokyonight
 --vim.cmd[[colorscheme tokyonight]]
 ----------------------------------------
---onedark
--- Lua
--- require('onedark').setup  {
---     -- Main options --
---     style = 'warmer', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
---     transparent = false,  -- Show/hide background
---     term_colors = true, -- Change terminal color as per the selected theme style
---     ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
---     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
---     -- toggle theme style ---
---     toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
---     toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
 
---     -- Change code style ---
---     -- Options are italic, bold, underline, none
---     -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
---     code_style = {
---         comments = 'italic',
---         keywords = 'none',
---         functions = 'none',
---         strings = 'none',
---         variables = 'none'
---     },
-
---     -- Lualine options --
---     lualine = {
---         transparent = false, -- lualine center bar transparency
---     },
-
---     -- Custom Highlights --
---     colors = {}, -- Override default colors
---     highlights = {}, -- Override highlight groups
-
---     -- Plugins Config --
---     diagnostics = {
---         darker = true, -- darker colors for diagnostic
---         undercurl = true,   -- use undercurl instead of underline for diagnostics
---         background = true,    -- use background color for virtual text
---     },
--- }
--- require('onedark').load()
------------------------------------
 --catppuccin
 require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
@@ -81,7 +40,7 @@ require("catppuccin").setup({
     no_bold = false, -- Force no bold
     styles = {
         comments = { "italic" },
-        conditionals = { "italic" },
+        conditionals = {},
         loops = {},
         functions = {},
         keywords = {},
@@ -105,23 +64,36 @@ require("catppuccin").setup({
         frappe = {},
         macchiato = {},
         mocha = {
-            blue = "#df4a16",
+            red = "#d20f39",
+            blue = "#d2a8f2",
             surface2 = "#9f9f9f",
             surface1 = "#777777",
             surface0 = "#272727",
+            mauve = "#ff7b72",
+            teal = "#7287fd",
             base = "#272727",
             mantle = "#000000",
             crust = "#ffffff",
-            -- green = "#df4a16"
+            green = "#a5d6f4",
             subtext1 = "#00ff00",
             subtext0 = "#0000ff",
             overlay2 = "#ffffff",
             overlay1 = "#0000ff",
-            overlay0 = "#ffffff",
-            lavender = "#ffffff"
+            overlay0 = "#888888",
+            lavender = "#ffffff",
+            yellow = "#ff7b72",
+            peach = "#d2a8ff",  -- "#7287fd", -- "#d2a8ff",
         },
     },
-    custom_highlights = {},
+
+    -- custom_highlights = function(colors)
+    --     return {
+    --         Comment = { fg = "#ffffff" },
+    --     }
+    -- end
+
+    -- custom_highlights = {
+    -- },
     integrations = {
         cmp = true,
         gitsigns = true,
